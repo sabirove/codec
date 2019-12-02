62241e3c8784c823523236e145cc8c48a9eafe2d
Evgenii Sabirov <sabirov.e@gmail.com>
2019-12-03 01:46:55 +0300

    Varint: - imporve modification notice - rearrange API - improve javadoc

--- src/main/java/sabirove/codec/util/Varint.java
+++ src/main/java/sabirove/codec/util/Varint.java
@@ -22,16 +22,23 @@ import java.io.*;
 import static sabirove.codec.util.CodecUtil.throwUnchecked;
 
 /*
- * adapted from: com.clearspring.analytics:stream:2.9.5 ~ com.clearspring.analytics.util.Varint
+ * Adapted from: com.clearspring.analytics:stream:2.9.5
+ * file: com.clearspring.analytics.util.Varint
  * github: https://github.com/addthis/stream-lib/blob/master/src/main/java/com/clearspring/analytics/util/Varint.java
+ *
+ * Modifications:
+ * 1. APIs adapted to accommodate InputStream/OutputStream parameters
+ * 2. Explicit EOF checks added for 'read' methods
+ * 3. Irrelevant API methods removed
+ * 4. IOException handling added
+ * 4. Javadoc adjusted accordingly
  */
 
 /**
  * LEB128 variable-length encoding/decoding utility.
  */
 public final class Varint {
-    private Varint() {
-    }
+    private Varint() { }
 
     /**
      * @see #writeUnsignedVarLong(long, OutputStream)
@@ -49,20 +56,11 @@ public final class Varint {
     }
 
     /**
-     * @see #writeSignedVarLong(long, OutputStream)
-     */
-    public static void writeSignedVarInt(int value, OutputStream out) {
-        // Great trick from http://code.google.com/apis/protocolbuffers/docs/encoding.html#types
-        writeUnsignedVarInt((value << 1) ^ (value >> 31), out);
-    }
-
-    /**
      * Read unsigned {@code int} written using the LEB128 variable-length encoding.
      *
-     * @throws IllegalArgumentException if variable-length value does not terminate
-     *                                  after 5 bytes have been read
-     * @throws UncheckedIOException     wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
-     * @throws UncheckedIOException     wrapping the {@link EOFException} if the end of stream is reached while reading
+     * @throws IllegalArgumentException if variable-length value does not terminate after 5 bytes have been read
+     * @throws UncheckedIOException     wrapping the {@link IOException} when operation fails with {@link IOException}
+     * @throws UncheckedIOException     wrapping the {@link EOFException} when the end of stream reached while reading
      * @see #readUnsignedVarLong(InputStream)
      */
     public static int readUnsignedVarInt(InputStream in) {
@@ -86,13 +84,21 @@ public final class Varint {
         }
     }
 
+
+    /**
+     * @see #writeSignedVarLong(long, OutputStream)
+     */
+    public static void writeSignedVarInt(int value, OutputStream out) {
+        // Great trick from http://code.google.com/apis/protocolbuffers/docs/encoding.html#types
+        writeUnsignedVarInt((value << 1) ^ (value >> 31), out);
+    }
+
     /**
      * Read signed {@code int} written using the "zig-zag" LEB128 variable-length encoding.
      *
-     * @throws IllegalArgumentException if variable-length value does not terminate
-     *                                  after 5 bytes have been read
-     * @throws UncheckedIOException     wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
-     * @throws UncheckedIOException     wrapping the {@link EOFException} if the end of stream is reached while reading
+     * @throws IllegalArgumentException if variable-length value does not terminate after 5 bytes have been read
+     * @throws UncheckedIOException     wrapping the {@link IOException} when operation fails with {@link IOException}
+     * @throws UncheckedIOException     wrapping the {@link EOFException} when the end of stream reached while reading
      * @see #readSignedVarLong(InputStream)
      */
     public static int readSignedVarInt(InputStream in) {
@@ -112,7 +118,7 @@ public final class Varint {
      *
      * @param value value to encode
      * @param out   to write bytes to
-     * @throws UncheckedIOException wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
+     * @throws UncheckedIOException wrapping the {@link IOException} when operation fails with {@link IOException}
      */
     public static void writeUnsignedVarLong(long value, OutputStream out) {
         try {
@@ -127,49 +133,13 @@ public final class Varint {
     }
 
     /**
-     * Write unsigned {@code long} using the "zig-zag" LEB128 variable-length encoding.
-     * If values are known to be potentially non-negative, {@link #writeUnsignedVarLong(long, OutputStream)}
-     * should be used instead.
-     *
-     * @param value value to encode
-     * @param out   to write bytes to
-     * @throws UncheckedIOException wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
-     */
-    public static void writeSignedVarLong(long value, OutputStream out) {
-        // Great trick from http://code.google.com/apis/protocolbuffers/docs/encoding.html#types
-        writeUnsignedVarLong((value << 1) ^ (value >> 63), out);
-    }
-
-    /**
-     * Read unsigned {@code long} written in the LEB128 variable-length encoding.
-     *
-     * @param in to read bytes from
-     * @return decoded value
-     * @throws UncheckedIOException     wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
-     * @throws UncheckedIOException     wrapping the {@link EOFException} if the end of stream is reached while reading
-     * @throws IllegalArgumentException if variable-length value does not terminate
-     *                                  after 9 bytes have been read
-     * @see #writeSignedVarLong(long, OutputStream)
-     */
-    public static long readSignedVarLong(InputStream in) {
-        long raw = readUnsignedVarLong(in);
-        // This undoes the trick in writeSignedVarLong()
-        long temp = (((raw << 63) >> 63) ^ raw) >> 1;
-        // This extra step lets us deal with the largest signed values by treating
-        // negative results from read unsigned methods as like unsigned values
-        // Must re-flip the top bit if the original read value had it set.
-        return temp ^ (raw & (1L << 63));
-    }
-
-    /**
      * Read signed {@code long} written in the "zig-zag" LEB128 variable-length encoding.
      *
      * @param in to read bytes from
      * @return decoded value
-     * @throws UncheckedIOException     wrapping the {@link IOException} if {@link OutputStream} throws {@link IOException}
-     * @throws UncheckedIOException     wrapping the {@link EOFException} if the end of stream is reached while reading
-     * @throws IllegalArgumentException if variable-length value does not terminate
-     *                                  after 9 bytes have been read
+     * @throws UncheckedIOException     wrapping the {@link IOException} when operation fails with {@link IOException}
+     * @throws UncheckedIOException     wrapping the {@link EOFException} when the end of stream reached while reading
+     * @throws IllegalArgumentException if variable-length value does not terminate after 9 bytes have been read
      * @see #writeUnsignedVarLong(long, OutputStream)
      */
     public static long readUnsignedVarLong(InputStream in) {
@@ -191,5 +161,39 @@ public final class Varint {
         } catch (IOException e) {
             return throwUnchecked(e);
         }
+    }
+
+    /**
+     * Write signed {@code long} using the "zig-zag" LEB128 variable-length encoding.
+     * If values are known to be potentially non-negative, {@link #writeUnsignedVarLong(long, OutputStream)}
+     * should be used instead.
+     *
+     * @param value value to encode
+     * @param out   to write bytes to
+     * @throws UncheckedIOException wrapping the {@link IOException} when operation fails with {@link IOException}
+     */
+    public static void writeSignedVarLong(long value, OutputStream out) {
+        // Great trick from http://code.google.com/apis/protocolbuffers/docs/encoding.html#types
+        writeUnsignedVarLong((value << 1) ^ (value >> 63), out);
+    }
+
+    /**
+     * Read unsigned {@code long} written in the LEB128 variable-length encoding.
+     *
+     * @param in to read bytes from
+     * @return decoded value
+     * @throws UncheckedIOException     wrapping the {@link IOException} when operation fails with {@link IOException}
+     * @throws UncheckedIOException     wrapping the {@link EOFException} when the end of stream reached while reading
+     * @throws IllegalArgumentException if variable-length value does not terminate after 9 bytes have been read
+     * @see #writeSignedVarLong(long, OutputStream)
+     */
+    public static long readSignedVarLong(InputStream in) {
+        long raw = readUnsignedVarLong(in);
+        // This undoes the trick in writeSignedVarLong()
+        long temp = (((raw << 63) >> 63) ^ raw) >> 1;
+        // This extra step lets us deal with the largest signed values by treating
+        // negative results from read unsigned methods as like unsigned values
+        // Must re-flip the top bit if the original read value had it set.
+        return temp ^ (raw & (1L << 63));
     }
 }
