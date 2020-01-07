14c1eb950c4f5ab0cbd82a5b2e424f21659e7e71
Evgenii Sabirov <sabirov.e@gmail.com>
2020-01-07 19:55:38 +0300

    improve com.github.sabirove.util.base64 docs

--- src/main/java/com/github/sabirove/codec/util/base64/package-info.java
+++ src/main/java/com/github/sabirove/codec/util/base64/package-info.java
@@ -15,19 +15,20 @@
  */
 
 /**
- * <p>This package contains adapted copy of the Apache commons-codec:commons-codec:1.13 library
- * truncated/refactored to get the working org.apache.commons.codec.binary.Base64InputStream implementation.
+ * <p>This package contains classes adapted from the Apache commons-codec:commons-codec:1.13 library
+ * to get working org.apache.commons.codec.binary.Base64InputStream implementation.
  *
- * <p>This is required until JDK-8222187 bug is fixed: https://bugs.openjdk.java.net/browse/JDK-8222187
- * {@link java.util.Base64.Decoder#wrap(java.io.InputStream)} stream adds unexpected null bytes at the
- * upon decoding (this trips the {@link javax.crypto.CipherInputStream} when it tries to read authentication
- * tag at the end of the stream).
+ * <p>This is required until JDK-8222187 bug is fixed: https://bugs.openjdk.java.net/browse/JDK-8222187:
+ * {@link java.util.Base64.Decoder#wrap(java.io.InputStream)} stream adds unexpected null bytes at the end
+ * upon decoding. This bug trips the {@link javax.crypto.CipherInputStream} when it tries to read and validate
+ * authentication tag at the end of the stream.
  * <br><br>
  * Modifications:
  * <ul>
  *  <li>removed interfaces/exception types</li>
  *  <li>replaced Charsets/Encoding related stuff (constants) with standard Java 1.7 spec constants</li>
- *  <li>package private / final for all except for Base64InputStream</li>
+ *  <li>removed some deprecated stuff</li>
+ *  <li>all final, all package private except for Base64InputStream</li>
  * </ul>
  */
 package com.github.sabirove.codec.util.base64;
